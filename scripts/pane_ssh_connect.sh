4859d68fa047e5904ea0dfa852485c599280c730
Tony-Sol <tony.kent.nar.earth@gmail.com>
2025-03-06 02:13:09 +0300

    fixes and improvements:
    - fixed podman/docker compatibility
    - improved getting current pane command
    - improved nested ssh session detection
    - add screencast into readme

--- scripts/pane_ssh_connect.sh
+++ scripts/pane_ssh_connect.sh
@@ -5,6 +5,17 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $CURRENT_DIR/shared.sh
 
 main() {
+	# @TODO user-formatted pane_ssh_connect output
+	# local info user host port
+	# info="$(get_info)"
+	# IFS='#' read -r user host port <<< "$info"
+	# # eval is literaly evil, so i won't use this
+	# # eval echo $(get_tmux_option $pane_ssh_connect_format)
+	# get_tmux_option $pane_ssh_connect_format | sed \
+	# 	-e "s/%{user}/$user/" \
+	# 	-e "s/%{host}/$host/" \
+	# 	-e "s/%{port}/$port/"
+
 	get_info
 }
 
